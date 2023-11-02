e2791b31f18534e2a84d4c9fd46f634634c5e464
Phong Lam <95520011+phonglam3103@users.noreply.github.com>
2023-11-02 16:32:25 +0100

    Add files via upload
    Add a modified version of gpt2.py to enquire multiple times unless "quit" is reached.

---
+++ gpt2_multiple_questions.py
@@ -0,0 +1,24 @@
+from transformers import AutoTokenizer, GPT2LMHeadModel, pipeline, BitsAndBytesConfig
+import torch
+import sys
+
+model_id = 'gpt2'
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model = GPT2LMHeadModel.from_pretrained(model_id,
+                                        device_map='auto',
+                                        offload_folder='offload',
+                                        low_cpu_mem_usage=True)
+print('Write your question, type "quit" to exit')
+while True:
+    q = input()
+    if (q=='quit'): sys.exit()
+    generate_text = pipeline(
+        model=model, tokenizer=tokenizer,
+        return_full_text=True,  # langchain expects the full text
+        task='text-generation',
+        max_new_tokens=512,  # mex number of tokens to generate in the output
+        repetition_penalty=1.1  # without this output begins repeating
+    )
+    res = generate_text(q)
+    print(res[0]["generated_text"])
