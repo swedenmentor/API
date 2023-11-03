8bba96dba0843d70e72a33d7d8b2382bdb3898d2
tuananhdao <kitsudo1412@gmail.com>
2023-11-03 10:43:57 +0100

    fix bug in serve.sh file

--- serve.sh
+++ serve.sh
@@ -1,7 +1,8 @@
 SSL_KEY_FILE=key.pem
 SSL_CERT_FILE=cert.pem
 
-if [ -f "$SSL_CERT_FILE"]; then
+if [ -f "$SSL_CERT_FILE" ]; then
     gunicorn --keyfile key.pem --certfile cert.pem --config gunicorn_config.py api:app
 else
     flask --app api run -h 0.0.0.0 -p 8000
+fi
