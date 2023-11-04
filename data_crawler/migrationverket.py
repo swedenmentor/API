345a5a02596d51fe99847730da8c32f615597790
Phong Lam <95520011+phonglam3103@users.noreply.github.com>
2023-11-04 14:29:52 +0100

    Update drafts for chunk storing

--- data_crawler/migrationverket.py
+++ data_crawler/migrationverket.py
@@ -3,18 +3,28 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+import json
+from datetime import datetime
 
-# %% 2️⃣ Extracting HTML from the website
+# Function to check if the url has been pre-crawled
 def check(url):
     global URL_list
     return (url in URL_list.keys())
 
+# Function to write data to a JSONL file
+def write_to_jsonl(data, filename):
+    with open(filename, 'a', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False)
+        file.write('\n')
+
+# %% 2️⃣ Extracting HTML from the website
 def crawl(url, depth):
     # Check if the URL was already crawled and depth reaches 0
-    if (depth == 0) or (check(url)):
-        return
+    if (depth == 0) or (check(url)): return
     global URL_list
     URL_list[url]=True
+    data ={}
+
     # Send an HTTP GET request to the URL
     response = requests.get(url)
 
@@ -31,8 +41,23 @@ def crawl(url, depth):
         # Extract content from p.normal and h2.subheading classes, store it in p_normal and h2_substring lists
         # Example link: https://www.migrationsverket.se/Privatpersoner/Arbeta-i-Sverige/Nyhetsarkiv/2023-11-01-Nu-borjar-det-hojda-forsorjningskravet-for-arbetstillstand-att-galla.html'''
         if url=='https://www.migrationsverket.se/Privatpersoner/Arbeta-i-Sverige/Nyhetsarkiv/2023-11-01-Nu-borjar-det-hojda-forsorjningskravet-for-arbetstillstand-att-galla.html':
+            #Extract date of update otherwise date = crawling date
+            date_tag = soup.find('p', class_='ahjalpfunktioner')
+            if date_tag: date = date_tag.get_text()
+            else: date = datetime.now().strftime("%Y-%m-%d")
+
+            # Extract data from p normal class or h2_subheading class
             p_normal = [p.get_text() for p in soup.find_all('p', class_='normal')]
             h2_subheading = [h2.get_text() for h2 in soup.find_all('h2', class_='subheading')]
+            for count, content in enumerate(p_normal):
+                data = {
+                    "source": url,
+                    "chunk-id": str(count),
+                    "title": soup.title.string,
+                    "chunk": content,
+                    "updated": date
+                }
+                write_to_jsonl(data, jsonl_file)
         # Find all links on the page
         for link in soup.find_all('a'):
             href = link.get('href')
@@ -51,9 +76,15 @@ def crawl(url, depth):
                 crawl(absolute_url, depth - 1)
 
 if __name__ == '__main__':
-    start_url = 'https://www.migrationsverket.se/'
+    start_url = 'https://www.migrationsverket.se/Privatpersoner/Arbeta-i-Sverige/Nyhetsarkiv/2023-11-01-Nu-borjar-det-hojda-forsorjningskravet-for-arbetstillstand-att-galla.html'
     max_depth = 10  # Set the maximum depth to control how many pages to crawl
     URL_list={}
+    jsonl_file = 'migrationsverket.jsonl'
+    
+    # Clear existing content in the JSONL file
+    with open(jsonl_file, 'w') as file:
+        file.write('')
+
     crawl(start_url, max_depth)
 
 
