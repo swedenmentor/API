e91159b0241a3aaf0b9d32076318ef47fcb9d852
Luong Nguyen Thanh <ph.ntluong95@gmail.com>
2023-11-06 10:11:37 +0100

    remove unused function and clean code

--- data_crawler/migrationverket.py
+++ data_crawler/migrationverket.py
@@ -8,13 +8,18 @@ from urllib.parse import urljoin                    # for URL parsing and joinin
 from googletrans import Translator                  # for text translation using Google Translate API
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-#from langdetect import detect, LangDetectException  # for language detection
 
 #%% 2️⃣ Define function to crawl data from the website
-def word_count(text):
-    return len(text.split())
 
 def chunk_text(input_text):
+    """
+    Split a text into chunks of a specific size with overlapping.
+
+    :param input_text: The input text to be chunked.
+    :type input_text: str
+    :return: A list of strings containing the chunks of the input text with overlapping.
+    :rtype: list[str]
+    """
     # This part is deposited for writing chunk_text from Langchain library
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = 1000,
@@ -23,19 +28,6 @@ def chunk_text(input_text):
         is_separator_regex = False
     )
     return text_splitter.split_text(input_text) # Return a list of strings with overlapping
-
-
-def check(url):
-    global URL_list
-    return (url in URL_list.keys())
-
-def neareast_next_full_stop(text):
-    pos = text.rfind('.')
-    if pos != -1:
-        return pos+1
-    else:
-        return len(text)
-
 
 class Crawler:
     """
@@ -46,10 +38,19 @@ class Crawler:
     translator (Translator): A Translator object to perform translations.
     """
     def __init__(self):
+        self.visited_urls = set() #! Set is a data structure that stores unique values
         self.translator = Translator()
         self.data_buffer = []
 
     def write_to_file(self, output_file):
+        """
+        Write data from data buffer to a file.
+
+        :param output_file: The path to the file where the data will be written.
+        :type output_file: str
+        :return: None
+        :rtype: None
+        """
         if self.data_buffer:
             with jsonlines.open(output_file, mode = 'a') as file:
                 for entry in self.data_buffer:
@@ -57,6 +58,17 @@ class Crawler:
                 self.data_buffer = []
         return
     def translate_text(self, text):
+        """
+        Translate Text
+
+        Translate the given text into English using a translator object.
+
+        :param text: The text to be translated.
+        :type text: str
+        :return: The translated text.
+        :rtype: list[str]
+
+        """
         output = []
         for chunk in chunk_text(text):
             detected = self.translator.detect(chunk)
@@ -90,14 +102,20 @@ class Crawler:
             requests.exceptions.ConnectionError: When a connection error occurs.
             Exception: For any other unhandled exceptions.
         """
-        if (depth == 0) or (check(url)): return
-        global URL_list
-        URL_list[url]=True
+
+        #! Check if the depth is 0 or the url has been visited
+        if (depth == 0) or (url in self.visited_urls):
+            return
+
         try:
+            #! Check connection first
             response = requests.get(url)
             if not response.status_code == 200:
                 print(f'Non success status for url {url}')
                 return
+            self.visited_urls.add(url) # Add url to visited_urls set
+
+            #! Extract web elmements
             soup = BeautifulSoup(response.text, 'html.parser')
             date_tag = soup.find('p', class_='ahjalpfunktioner')
             if date_tag:
@@ -126,7 +144,10 @@ class Crawler:
                 self.data_buffer.append(entries[key])
             self.write_to_file(output_file)
 
+            #! Recursion
+            # Find all links on the page
             links = soup.find_all('a')
+            # Recursively crawl each of the links found on the page
             for link in links:
                 href = link.get('href')
                 if href and href.startswith('http'):
@@ -137,6 +158,7 @@ class Crawler:
                     time.sleep(1)
                     self.crawl_website(new_url, output_file, depth=depth - 1)
 
+        #! Give error message when connection fails
         except requests.exceptions.RequestException as err:
             print(f"RequestException: {err}")
         except requests.exceptions.HTTPError as errh:
@@ -153,16 +175,11 @@ class Crawler:
 if __name__ == '__main__':
     # Initialize models and variables
     crawler = Crawler()
-    # text_splitter = RecursiveCharacterTextSplitter(
-    #     chunk_size = 1000, chunk_overlap  = 200,
-    #     length_function = len, is_separator_regex = False)
     max_depth = 5
     url = 'https://www.migrationsverket.se/Privatpersoner/Arbeta-i-Sverige/Nyhetsarkiv/2023-11-01-Nu-borjar-det-hojda-forsorjningskravet-for-arbetstillstand-att-galla.html'
     output_file = 'migrationverket.jsonl'
-    URL_list={}
+
+
+
     # Crawling
     crawler.crawl_website(url, output_file=output_file, depth=max_depth)
-
-    # # make sure to write remaining data to file
-    # if crawler.data_buffer:
-    #     crawler.write_to_file(output_file)
