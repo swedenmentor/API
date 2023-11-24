70f508d33a3a6514081e26b0555f39e83d6e1ddd
tuananhdao <kitsudo1412@gmail.com>
2023-11-24 15:07:10 +0100

    update dependencies

--- serve.sh
+++ serve.sh
@@ -3,8 +3,8 @@ SSL_CERT_FILE=cert.pem
 
 if [ -f "$SSL_CERT_FILE" ]; then
     #gunicorn --keyfile key.pem --certfile cert.pem --config gunicorn_config.py api:app
-    hypercorn --keyfile $SSL_KEY_FILE --certfile $SSL_CERT_FILE stream_api:app
+    hypercorn --keyfile $SSL_KEY_FILE --certfile $SSL_CERT_FILE --bind 0.0.0.0:8000 --workers 2 stream_api:app
 else
     #flask --app api run -h 0.0.0.0 -p 8000
-    hypercorn stream_api:app
+    hypercorn --bind 0.0.0.0:8000 --reload stream_api:app
 fi
