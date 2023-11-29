667b96181b3dbf81e3ab4098e965c32a7fa25d79
tuananhdao <kitsudo1412@gmail.com>
2023-11-29 14:41:34 +0100

    fix stream newline

--- stream_api.py
+++ stream_api.py
@@ -24,8 +24,8 @@ async def run(prompt):
         generate_text.arun(prompt),
         stream_callback.done)
     )
-    yield '{"choices": [{"index": 0, "delta": {"content": " end"}, "context": {"followup_questions": [], "data_points": []}, "session_state": null}]}'
-    await asyncio.sleep(0.1)
+    yield '{"choices": [{"index": 0, "delta": {"content": " end"}, "context": {"followup_questions": [], "data_points": []}, "session_state": null}]}\n'
+    #await asyncio.sleep(0.1)
     batch = 5
     iter = 0
     batch_string = ''
@@ -33,24 +33,24 @@ async def run(prompt):
         #print(token)
         # Use server-sent-events to stream the response
         if iter == batch:
-            yield '{"choices": [{"index": 0, "delta": {"content": "'+batch_string+'"}, "context": {"followup_questions": []}, "session_state": null}]}'
+            print(batch_string)
+            yield '{"choices": [{"index": 0, "delta": {"content": "'+batch_string+'"}, "context": {"followup_questions": []}, "session_state": null}]}\n'
             batch_string = ''
             iter = 0
         batch_string += token
         iter += 1
+    print(batch_string)
+    yield '{"choices": [{"index": 0, "delta": {"content": "'+batch_string+'"}, "context": {"followup_questions": []}, "session_state": null}]}\n'
 
     await task
     #stream_callback.done.clear()
-    await asyncio.sleep(0.1)
-    yield '{"choices": [{"index": 0, "delta": {"content": "'+batch_string+'"}, "context": {"followup_questions": []}, "session_state": null}]}'
-    await asyncio.sleep(0.1)
+    #await asyncio.sleep(0.1)
     yield '{"choices": [{"index": 0, "delta": {}, "context": {"followup_questions": []}, "session_state": null}]}'
     
-    # yield '{"choices": [{"index": 0, "delta": {"content": " end"}, "context": {"followup_questions": [], "data_points": []}, "session_state": null}]}'
-    # await asyncio.sleep(0.1)
+    # yield '{"choices": [{"index": 0, "delta": {"content": " end"}, "context": {"followup_questions": [], "data_points": []}, "session_state": null}]}\n'
     # for i in range(100):
-    #     yield '{"choices": [{"index": 0, "delta": {"content": "'+str(i)+'?aa "}, "context": {"followup_questions": []}, "session_state": null}]}'
-    #     await asyncio.sleep(0.1)
+    #     print(i)
+    #     yield '{"choices": [{"index": 0, "delta": {"content": "'+str(i)+'?aa "}, "context": {"followup_questions": []}, "session_state": null}]}\n'
     # yield '{"choices": [{"index": 0, "delta": {}, "context": {"followup_questions": []}, "session_state": null}]}'
 
 @app.post("/q")
